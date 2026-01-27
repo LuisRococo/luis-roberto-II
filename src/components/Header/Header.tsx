3783193d460622a4d5ce41495fd964edf9960eea
LuisRococo <luisrobertopenav@gmail.com>
2026-01-27 13:31:48 -0700

    add max size to container, so the website looks cool on tvs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,11 +3,17 @@ import avatarImg from "../../assets/images/profile-img.jpg";
 
 const Header = () => {
   return (
-    <div className={`${styles.header} container`}>
-      <div className={styles.content}>
-        <img className={styles.avatar} src={avatarImg} alt="profile picture" />
-        <h1 className={styles.name}>Luis Roberto Peña</h1>
-        <p className={styles.career}>Software Engineer</p>
+    <div className={styles.header}>
+      <div className="container">
+        <div className={styles.content}>
+          <img
+            className={styles.avatar}
+            src={avatarImg}
+            alt="profile picture"
+          />
+          <h1 className={styles.name}>Luis Roberto Peña</h1>
+          <p className={styles.career}>Software Engineer</p>
+        </div>
       </div>
     </div>
   );
